36c9098534e15f2d2da9220d687a509a147cb49c
josephajibodu <josephajibodu@gmail.com>
2025-09-18 20:20:51 +0100

    design system complete

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,14 @@
 export default function LoginPage() {
   return (
     <div className="bg-gray-950 h-screen text-white">
-      Login form with the new font
+      <p className="text-preset-1">Login form with the new font</p>
+      <p className="text-preset-2">Login form with the new font</p>
+      <p className="text-preset-3">Login form with the new font</p>
+      <p className="text-preset-3-bold">Login form with the new font</p>
+      <p className="text-preset-3-semibold">Login form with the new font</p>
+      <p className="text-preset-4 text-purple-600">
+        Login form with the new font
+      </p>
     </div>
   );
 }
