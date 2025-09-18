552ac68de78863cbb027f2240344e94e218dc678
josephajibodu <josephajibodu@gmail.com>
2025-09-18 19:46:39 +0100

    fix register route

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router";
 import LoginPage from "./pages/login";
+import RegisterPage from "./pages/register";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
-      <Route path="/register" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
     </Routes>
   );
 }
