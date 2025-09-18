1a50133709b5d8b9fc9cff703bae1aec40590c9f
josephajibodu <josephajibodu@gmail.com>
2025-09-18 19:45:55 +0100

    react router setup commplete
    login and register page addecd

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,11 @@
+import { Route, Routes } from "react-router";
+import LoginPage from "./pages/login";
+
 export default function App() {
-  return <div className="bg-red-300 h-screen">App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/register" element={<LoginPage />} />
+    </Routes>
+  );
 }
