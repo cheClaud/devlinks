e49302cedae0f0c5f85d2faef87709c5da19a754
Che Claud <tamanjiche@gmail.com>
2025-09-20 06:29:55 +0100

    unstyled login page with form inputs and icons created

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,14 +1,64 @@
+import React, { useState } from "react";
+import LargeLogo from '../assets/images/logo-devlinks-large.svg'
+import EmailIcon from "../assets/images/icon-email.svg";
+import PasswordIcon from "../assets/images/icon-password.svg";
+
 export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // Login logic will stay empty for now
+    console.log("Email:", email, "Password:", password);
+  };
   return (
-    <div className="bg-gray-950 h-screen text-white">
-      <p className="text-preset-1">Login form with the new font</p>
-      <p className="text-preset-2">Login form with the new font</p>
-      <p className="text-preset-3">Login form with the new font</p>
-      <p className="text-preset-3-bold">Login form with the new font</p>
-      <p className="text-preset-3-semibold">Login form with the new font</p>
-      <p className="text-preset-4 text-purple-600">
-        Login form with the new font
-      </p>
+    <div className="flex flex-col justify-center items-center bg-gray-50 h-screen ">
+      <img src={LargeLogo} alt="DevLinks Logo" className="pb-[3rem]"/>
+      <div className="p-[2.5rem] bg-white rounded-xl">
+        <div className="pb-[2.5rem]">
+          <p>Login</p>
+          <p>Add your details below to get back into the app</p>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="email">Email address</label>
+            <div className="relative">
+              <div>
+                <img src={EmailIcon} alt="Email icon" className="" />
+              </div>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <div className="relative">
+              <div>
+                <img
+                  src={PasswordIcon}
+                  alt="Password icon"
+                  className=""
+                />
+              </div>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+          </div>
+          <button type="submit">Login</button>
+        </form>
+        <p>Don't have an account? Create an account.</p>
+      </div>
     </div>
   );
 }
