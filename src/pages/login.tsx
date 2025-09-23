773f083ca7ef46c4c89c2b730c67341e31b12435
josephajibodu <josephajibodu@gmail.com>
2025-09-23 19:09:52 +0100

    fixed the layout on login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import LargeLogo from '../assets/images/logo-devlinks-large.svg'
+import LargeLogo from "../assets/images/logo-devlinks-large.svg";
 import EmailIcon from "../assets/images/icon-email.svg";
 import PasswordIcon from "../assets/images/icon-password.svg";
 import { Link } from "react-router";
@@ -14,25 +14,21 @@ export default function LoginPage() {
     console.log("Email:", email, "Password:", password);
   };
   return (
-    <div className="flex flex-col justify-center items-center bg-gray-50 h-screen py-[13rem] px-30rem ">
-      <div className="p-[2.5rem] pb-[3rem] w-full">
-        <img
-          src={LargeLogo}
-          alt="DevLinks Logo"
-          className="block ml-0 md:mx-auto"
-        />
-      </div>
+    <div className="flex flex-col md:justify-center items-start md:items-center gap-y-16 md:gap-y-12 bg-gray-50 h-screen py-8 px-8">
+      {/* Logo Container */}
+      <img src={LargeLogo} alt="DevLinks Logo" className="h-10" />
 
-      <div className="p-[2.5rem] bg-white rounded-xl w-[30rem]">
-        <div className="pb-[2.5rem]">
+      {/* Form Container */}
+      <div className="p-0 md:p-10 md:bg-white rounded-xl w-full md:w-119">
+        <div className="pb-10">
           <p className="text-preset-1">Login</p>
           <p className="text-preset-4 ">
             Add your details below to get back into the app
           </p>
         </div>
         <form onSubmit={handleSubmit}>
-          <div className="mb-[1.5rem]">
-            <label htmlFor="email" className="mb-[0.5rem]">
+          <div className="mb-6">
+            <label htmlFor="email" className="mb-2">
               Email address
             </label>
             <div className="relative">
@@ -40,7 +36,9 @@ export default function LoginPage() {
                 <img src={EmailIcon} alt="Email icon" className="" />
               </div>
               <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
-                <p className="text-xs text-gray-400 invisible">Can't be empty</p>
+                <p className="text-xs text-gray-400 invisible">
+                  Can't be empty
+                </p>
               </div>
               <input
                 type="email"
@@ -49,12 +47,12 @@ export default function LoginPage() {
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 required
-                className="w-full pl-10 p-[0.5rem] border border-gray-300 rounded-[0.5rem]"
+                className="w-full pl-10 p-2 border border-gray-300 rounded-lg"
               />
             </div>
           </div>
-          <div className="mb-[1.5rem]">
-            <label htmlFor="password" className="mb-[0.5rem]">
+          <div className="mb-6">
+            <label htmlFor="password" className="mb-">
               Password
             </label>
             <div className="relative">
@@ -62,7 +60,9 @@ export default function LoginPage() {
                 <img src={PasswordIcon} alt="Password icon" className="" />
               </div>
               <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
-                <p className="text-xs text-gray-400 invisible">Please check again</p>
+                <p className="text-xs text-gray-400 invisible">
+                  Please check again
+                </p>
               </div>
               <input
                 type="password"
@@ -71,13 +71,13 @@ export default function LoginPage() {
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 required
-                className="w-full pl-10 p-2 border border-gray-300 rounded-[0.5rem]"
+                className="w-full pl-10 p-2 border border-gray-300 rounded-lg"
               />
             </div>
           </div>
           <button
             type="submit"
-            className="mb-[1.5rem] w-full text-white text-center rounded-[0.5rem] p-2 cursor-pointer bg-purple-600"
+            className="mb-6 w-full text-white text-center rounded-lg p-2 cursor-pointer bg-purple-600"
           >
             Login
           </button>
