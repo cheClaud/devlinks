a8baca060bbcaef44442b531a98c371cd46d9a66
josephajibodu <josephajibodu@gmail.com>
2025-10-15 19:45:05 +0100

    button with variant

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,19 +1,39 @@
 import type { ComponentProps } from "react";
 
+interface ButtonProps extends ComponentProps<"button"> {
+  variant?: "primary" | "secondary";
+}
+
 export default function Button({
   children,
   className,
+  variant = "primary",
   ...props
-}: ComponentProps<"button">) {
+}: ButtonProps) {
+  console.log(variant);
+  let customClassNames = "hover:cursor-pointer rounded-lg py-4 px-6 ";
+
+  // when variant = primary
+  // bgcolor purple 600
+  // textcolor white
+  if (variant === "primary") {
+    customClassNames +=
+      "bg-purple-600 hover:bg-purple-300 disabled:bg-purple-600/25 text-white";
+  }
+
+  // when variant = secondary
+  // bgcolor transparent
+  // textcolor purple 600
+  // border purple 600
+  if (variant === "secondary") {
+    customClassNames +=
+      "bg-transparent border border-purple-600 hover:bg-[#EFEBFF] disabled:opacity-25 text-purple-600";
+  }
+
+  customClassNames += " " + className;
+
   return (
-    <button
-      {...props}
-      className={
-        "bg-purple-600 hover:bg-purple-300 hover:cursor-pointer disabled:bg-purple-600/25 text-white rounded-lg py-4 px-6" +
-        " " +
-        className
-      }
-    >
+    <button {...props} className={customClassNames}>
       {children}
     </button>
   );
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -105,7 +105,7 @@ export default function DashoardPage() {
 
           {/*footer container*/}
           <div className="flex justify-end items-center px-4 md:px-10 py-4 md:py-6">
-            <Button variant="primary">Save</Button>
+            <Button disabled>Save</Button>
           </div>
         </div>
       </main>
