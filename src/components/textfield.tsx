86e27d3f3356740d75dba5f21261bc48ab1067c8
Che Claud <tamanjiche@gmail.com>
2025-10-22 16:23:32 +0100

    added hasicon prop and modified custom classes

--- src/components/textfield.tsx
+++ src/components/textfield.tsx
@@ -4,17 +4,19 @@ import CopyLink from ".././assets/images/icon-link-copied-to-clipboard.svg";
 
 interface TextFieldProps extends ComponentProps<"input"> {
   error?: boolean;
+  hasIcon: boolean;
   errorMessage?: string;
 }
 
 export default function TextField({
   className,
+  hasIcon = true,
   error = false,
   errorMessage = "Can't be empty",
   ...props
 }: TextFieldProps) {
   const inputClasses = cn(
-    "w-full border border-gray-300 bg-white rounded-lg py-4 pl-10 pr-4 outline-none focus:border-purple-600 focus:shadow-lg focus:shadow-purple-600/20",
+    `w-full border border-gray-300 bg-white rounded-lg py-4 ${hasIcon? 'pl-10': 'pl-2'} pr-4 outline-none focus:border-purple-600 focus:shadow-lg focus:shadow-purple-600/20`,
     {
       "border-red-500 focus:border-red-500": error,
     },
@@ -25,9 +27,11 @@ export default function TextField({
     <div className="w-120 p-0">
       <div className="relative">
         {/* Icon on the left */}
-        <div className="absolute inset-y-0 left-0 pl-4 flex items-center pointer-events-none">
-          <img src={CopyLink} alt="copied link icon" className="w-4 h-4" />
-        </div>
+        {hasIcon && (
+          <div className="absolute inset-y-0 left-0 pl-4 flex items-center pointer-events-none">
+            <img src={CopyLink} alt="copied link icon" className="w-4 h-4" />
+          </div>
+        )}
 
         {/* Error message on the right */}
         {error && (
