9f6777071d645616af6a73a8d8a27682b2c0ed02
josephajibodu <josephajibodu@gmail.com>
2025-10-22 19:55:06 +0100

    textfield refactored

--- src/components/textfield.tsx
+++ src/components/textfield.tsx
@@ -1,22 +1,23 @@
-import type { ComponentProps } from "react";
+import type { ComponentProps, ReactElement } from "react";
 import { cn } from "../libs/utils";
-import CopyLink from ".././assets/images/icon-link-copied-to-clipboard.svg";
 
 interface TextFieldProps extends ComponentProps<"input"> {
   error?: boolean;
-  hasIcon: boolean;
+  icon?: ReactElement;
   errorMessage?: string;
 }
 
 export default function TextField({
   className,
-  hasIcon = true,
+  icon,
   error = false,
   errorMessage = "Can't be empty",
   ...props
 }: TextFieldProps) {
   const inputClasses = cn(
-    `w-full border border-gray-300 bg-white rounded-lg py-4 ${hasIcon? 'pl-10': 'pl-2'} pr-4 outline-none focus:border-purple-600 focus:shadow-lg focus:shadow-purple-600/20`,
+    `w-full border border-gray-300 bg-white rounded-lg py-4 ${
+      icon ? "pl-10" : "pl-4"
+    } pr-4 outline-none focus:border-purple-600 focus:shadow-lg focus:shadow-purple-600/20`,
     {
       "border-red-500 focus:border-red-500": error,
     },
@@ -24,12 +25,12 @@ export default function TextField({
   );
 
   return (
-    <div className="w-full md:w-120 p-0">
+    <div className="w-full p-0">
       <div className="relative">
         {/* Icon on the left */}
-        {hasIcon && (
+        {icon && (
           <div className="absolute inset-y-0 left-0 pl-4 flex items-center pointer-events-none">
-            <img src={CopyLink} alt="copied link icon" className="w-4 h-4" />
+            {icon}
           </div>
         )}
 
